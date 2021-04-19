2347f15fe53f87651bbfd2661602c72f0d5e53a9
Harold Dost <harolddost@gmail.com>
2021-04-20 00:37:06 +0200

    Enabled Tag Parsing Support
    * Graphite Tags are now owned by the struct.
    * Fix Graphite Regex
    * Add separate tag regex for iteration.

--- lib/carbon/src/lib.rs
+++ lib/carbon/src/lib.rs
@@ -8,7 +8,7 @@ extern crate lazy_static;
 // tested.
 pub struct GraphiteDataPoint<'a> {
     pub path:&'a str,
-    pub tags:HashMap<&'a str,&'a str>,
+    pub tags:HashMap<String,String>,
     pub value:f64,
     pub timestamp:Option<u32>,
 }
@@ -21,13 +21,21 @@ impl<'a> GraphiteDataPoint<'a> {
 
 fn parse_graphite(input:&str) -> Result<GraphiteDataPoint, &'static str> {
     lazy_static!{
-        static ref RE: Regex = Regex::new(r"^(?P<path>[^;[:space:]]+)(?:;(?P<tag_key>[^;!\^=[:space:]]]+)=(?P<tag_value>[^;~[:space:]][^;[:space:]]*))*(?: (?P<value>\d+))(?: (?P<time>\d+)){0,1}").unwrap();
+        static ref RE: Regex = Regex::new(r"^(?P<path>[^;[:space:]]+)(?P<tags>(?:;(?P<tag_name>[^;=\^!]+)=(?P<tag_value>[^;~][^;[:space:]]+))*)(?: (?P<value>\d+))(?: (?P<time>\d+)){0,1}").unwrap();
+        static ref TAG: Regex = Regex::new(r";(?P<tag_name>[^;=\^!]+)=(?P<tag_value>[^;~][^;]+)").unwrap();
     }
     match RE.captures(input) {
         Some(cap) => {
             println!("{:?}",cap);
             let path = cap.name("path").unwrap().as_str();
             let value = cap.name("value").unwrap().as_str().parse::<f64>().unwrap();
+            let tags = cap.name("tags").unwrap();
+            let mut tag_map = HashMap::new();
+            println!("Tags: {:?}",tags);
+            for tag in TAG.captures_iter(tags.as_str()) {
+                tag_map.insert(tag[1].to_string(),tag[2].to_string());
+            }
+
             let time = cap.name("time");
             let timestamp = match time {
                 Some(value) => {
@@ -35,7 +43,7 @@ fn parse_graphite(input:&str) -> Result<GraphiteDataPoint, &'static str> {
                 }
                 _ => None
             };
-            Ok(GraphiteDataPoint{path,tags:HashMap::new(),value,timestamp})
+            Ok(GraphiteDataPoint{path,tags:tag_map,value,timestamp})
         }
         _ => Err("Input line doesn't match"),
 
@@ -71,44 +79,40 @@ mod tests {
     }
 
     #[test]
-    #[ignore]
     fn parse_tagged_path() {
-        let output = super::parse_graphite("simple.path;tag=value 1234");
+        let output = super::parse_graphite("tagged.path;tag=value 1234");
         match output {
             Ok(valid) => {
-                assert_eq!(valid.path, "simple.path");
+                assert_eq!(valid.path, "tagged.path");
                 assert_eq!(valid.value, 1234.0);
                 assert_eq!(valid.tags.len(),1);
                 assert_eq!(valid.tags["tag"],"value");
-                assert_eq!(valid.timestamp, Some(4567));
             },
             _ => { panic!() },
         }
     }
 
     #[test]
-    #[ignore]
     fn parse_multi_tagged_path() {
-        let output = super::parse_graphite("simple.path;tag=value;second=tag 1234");
+        let output = super::parse_graphite("multi.tagged.path;tag=value;second=tag 1234");
         match output {
             Ok(valid) => {
-                assert_eq!(valid.path, "simple.path");
+                assert_eq!(valid.path, "multi.tagged.path");
                 assert_eq!(valid.value, 1234.0);
                 assert_eq!(valid.tags.len(),2);
-                assert_eq!(valid.timestamp, Some(4567));
             },
             _ => { panic!() },
         }
     }
 
     #[test]
-    #[ignore]
     fn parse_tagged_path_with_ts() {
         let output = super::parse_graphite("simple.path;tag=value 1234 4567");
         match output {
             Ok(valid) => {
                 assert_eq!(valid.path, "simple.path");
                 assert_eq!(valid.value, 1234.0);
+                assert_eq!(valid.tags.len(),1);
                 assert_eq!(valid.timestamp, Some(4567));
             },
             _ => { panic!() },
